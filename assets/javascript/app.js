1a05d6589a4c81c35137d8fb73b7663489d98976
unknown <jjhernandez94@yahoo.com>
2018-01-26 23:56:56 -0800

    Trivia Game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,35 @@
-
-//When button is pressed, start time function(stopwatch.start, stopwatch.count, then stopwatch.timeConverter) and game function(stopwatch.gameQAndA)
-
-//When t = 0, run reset function (t = 30), run game function again IF for loop iteration is not done
-
-
-//STOP WATCH
 window.onload = function() {
- $("#start").on("click", stopwatch.startClock);
- $("#start").on("click", stopwatch.startTriviaGame);
+  $("#start").on("click", function() {
+    $(this).hide();
+    nextQuestion();
+  });
+  $("#start").on("click", stopwatch.start);
+}
 
-};
+var showQuestion;
+var questionNumber = 0;
 var intervalId;
 var clockRunning = false;
+var count = 0;
 
-// Our stopwatch object
+/////////////////////////////////////////////////////////////////////////
+//Stop watch object
 var stopwatch = {
+
   time: 30,
+  lap: 1,
 
   reset: function() {
 
-    stopwatch.time = 30;
-    // stopwatch.lap = 1;
+    stopwatch.time = 5;
 
     // DONE: Change the "display" div to "00:00."
-    $("#display").text("00:00");	
+    $("#display").text("00:00");
+  },
+  start: function() {
 
-  startClock: function() {
     // DONE: Use setInterval to start the count here and set the clock to running.
     if (!clockRunning) {
-        
         intervalId = setInterval(stopwatch.count, 1000);
         clockRunning = true;
     }
@@ -40,82 +41,134 @@ var stopwatch = {
     clockRunning = false;
   },
   count: function() {
+
+    // DONE: increment time by 1, remember we cant use "this" here.
     stopwatch.time--;
+
+    // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+    //       and save the result in a variable.
     var converted = stopwatch.timeConverter(stopwatch.time);
     console.log(converted);
+
+    // DONE: Use the variable we just created to show the converted time in the "display" div.
     $("#display").text(converted);
   },
+  
   timeConverter: function(t) {
+
     var minutes = Math.floor(t / 60);
     var seconds = t - (minutes * 60);
+
     if (seconds < 10) {
       seconds = "0" + seconds;
     }
+
     if (minutes === 0) {
       minutes = "00";
-    }
-  //figure out how to make this work
-    if (seconds === 0 && minutes === 0) {
-    	stopwatch.reset;
-
     }
     else if (minutes < 10) {
       minutes = "0" + minutes;
     }
+
     return minutes + ":" + seconds;
   }
 };
 
-var questions = {
-    Q : ["question1?", "question2?", "question3?", "question4?"],
-    A : [answers1[answer1placeholder, answer2placeholder, answer3placeholder, answer4placeholder], 
-        answers2[answer1placeholder, answer2placeholder, answer3placeholder, answer4placeholder],
-        answers3[answer1placeholder, answer2placeholder, answer3placeholder, answer4placeholder],
-        answers4[answer1placeholder, answer2placeholder, answer3placeholder, answer4placeholder]]
-}  
-
-var count2 = 0;
-
-function displayQAndA() {
-  //for loop that takes the question based on the count(index)
-  for (var index = 0; index <= questions.Q.length; index++) { 
-    $("#question-holder").html(questions.Q[index]);
-    $("#answer1-holder").html(questions.A[index]answers[0]);
-    $("#answer2-holder").html(questions.A[index]answers[1]);
-    $("#answer3-holder").html(questions.A[index]answers[2]);
-    $("#answer4-holder").html(questions.A[index]answers[3]);
+var questions = [
+  {
+    question: 'Who is third behind Hank Aaron and Babe Ruth in major league career home runs?',
+    choices: ['Reggie Jackson', 'Harmon Killebrew', 'Willie Mays', 'Frank Robinson'],
+    answer: 'Willie Mays'
+  },
+  {
+    question: 'What did the "D" in "D-Day" stand for?',
+    choices: ['doom', 'day', 'Dwight (Eisenhower)', 'Dunkirk'],
+    answer: 'day'
+  },
+  {
+    question: 'Edward Scissorhands was set in which time period?',
+    choices: ['The 1960s', 'The 1970s', 'The 1950s', 'The 1980s'],
+    answer: 'The 1950s'
+  },
+  {
+    question: 'Kylie Minogue began her music career in the late 80s. Which one of the following has she not done a duet with?',
+    choices: ['Jimmy Plant', 'Nick Cave', 'Coldplay', 'Shakira'],
+    answer: 'Shakira'
+  },
+  {
+    question: 'What does the Halloween color orange represent?',
+    choices: ['The harvest', 'The twilight', 'The burning of spirits', 'The voice of spirits'],
+    answer: 'The harvest'
+  },
+  {
+    question: 'What was the trick originally in ‘trick or treat’?',
+    choices: ['Knock on the door and then run away', 'Throw water at the person opening the door', 'Sing a song to the person giving you the candy', 'Recite a good luck protection spell'],
+    answer: 'Sing a song to the person giving you the candy'
+  },
+  {
+    question: 'What does ponophobia mean?',
+    choices: ['The fear of overheating', 'The fear of oversleeping', 'The fear of overthinking', 'The fear of overworking'],
+    answer: 'The fear of overworking'
+  },
+  {
+    question: 'Indiana Jones’ dad was obsessed with finding what?',
+    choices: ['The Holy Scriptures', 'The Holy Grail', 'The Holy Cross', 'The Holy Temple'],
+    answer: 'The Holy Grail'
+  },
+  {
+    question: 'Which one of these was the daughter of Cliff Huxtable (the dad from The Cosby Show)?',
+    choices: ['Denise', 'Donna', 'Diane', 'Danielle'],
+    answer: 'Denise'
+  },
+  {
+    question: 'Which character was the second to be disqualified in ‘Charlie and the Chocolate Factory’?',
+    choices: ['Augustus Gloop', 'Violet Beauregarde', 'Mike Teavee', 'Veruca Salt'],
+    answer: 'Violet Beauregarde'
   }
-}
+]
 
+
+
+
+
+// nextQuestion();
 function nextQuestion() {
-  //  TODO: Increment the count by 1.
-  count2++;
-
-  $("#image-holder").html("<img src='images/loading.gif' width='200px'/>");
-  // TODO: Use a setTimeout to run displayImage after 1 second.
-  setTimeout(displayQAndA, 1000);
-
-
-  // TODO: If the count is the same as the length of the question array, reset the count to 0.
-  if (count2 === stopwatch.length) {
-    count2 = 0;
+  // debugger;
+  
+  $('#choices').empty();
+  var currentQuestion = questions[questionNumber];
+  $('#question').text(currentQuestion.question);
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    var choice = currentQuestion.choices[i];
+    var button = $('<button>'); 
+    button.addClass('btn btn-primary choice');
+    button.text(choice);
+    button.attr('data-choice', choice);
+    $('#choices').append(button);
   }
 }
 
-function startTriviaGame() {
+if (stopwatch.time == 0) {
+    debugger;
+     $(".choices").html('Times Up!')
+     nextQuestion();
+     questionNumber++;
+     stopwatch.reset;
+  } 
 
-  // TODO: Use showImage to hold the setInterval to run nextImage.
-  showQuestion = setInterval(nextQuestion, stopwatch.time);
-
-}
-
-function stopTriviaGame() {
-
-  // TODO: Put our clearInterval here:
-  clearInterval(showQuestion);
-
-}
-
-// This will run the display image function as soon as the page loads.
-displayQAndA();
+$(document).on('click', '.choice', function() {
+  debugger;
+  var choice = $(this).attr('data-choice');
+  if (choice == questions[questionNumber].answer) {
+    questionNumber++;
+    stopwatch.reset;
+  } 
+  else {
+    $("#choices").html('Wrong!')
+    console.log('Wrong!');
+    stopwatch.reset;
+    showQuestion = setInterval(nextQuestion, 1000);
+    nextQuestion();
+  }
+})
 
---
+++ assets/javascript/app2.js
@@ -0,0 +1,121 @@
+
+//When button is pressed, start time function(stopwatch.start, stopwatch.count, then stopwatch.timeConverter) and game function(stopwatch.gameQAndA)
+
+//When t = 0, run reset function (t = 30), run game function again IF for loop iteration is not done
+
+
+//STOP WATCH
+window.onload = function() {
+ $("#start").on("click", stopwatch.startTimer);
+ $("#start").on("click", stopwatch.startTriviaGame);
+
+};
+var intervalId;
+var clockRunning = false;
+
+// Our stopwatch object
+var stopwatch = {
+  time: 30,
+
+  reset: function() {
+
+    stopwatch.time = 30;
+    // stopwatch.lap = 1;
+
+    // DONE: Change the "display" div to "00:00."
+    $("#display").text("00:00");	
+
+  startTimer: function() {
+    // DONE: Use setInterval to start the count here and set the clock to running.
+    if (!clockRunning) {
+        
+        intervalId = setInterval(stopwatch.count, 1000);
+        clockRunning = true;
+    }
+  },
+  stop: function() {
+
+    // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+    clearInterval(intervalId);
+    clockRunning = false;
+  },
+  count: function() {
+    stopwatch.time--;
+    var converted = stopwatch.timeConverter(stopwatch.time);
+    console.log(converted);
+    $("#display").text(converted);
+  },
+  timeConverter: function(t) {
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    if (minutes === 0) {
+      minutes = "00";
+    }
+  //figure out how to make this work
+    if (seconds === 0 && minutes === 0) {
+    	stopwatch.reset;
+
+    }
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    return minutes + ":" + seconds;
+  }
+};
+
+var questions = {
+    Q : ["question1?", "question2?", "question3?", "question4?"],
+    A : [answers1[answer1placeholder, answer2placeholder, answer3placeholder, answer4placeholder], 
+        answers2[answer1placeholder, answer2placeholder, answer3placeholder, answer4placeholder],
+        answers3[answer1placeholder, answer2placeholder, answer3placeholder, answer4placeholder],
+        answers4[answer1placeholder, answer2placeholder, answer3placeholder, answer4placeholder]]
+}  
+
+var count2 = 0;
+
+function displayQAndA() {
+  //for loop that takes the question based on the count(index)
+  for (var index = 0; index <= questions.Q.length; index++) { 
+    $("#question-holder").html(questions.Q[index]);
+    $("#answer1-holder").html(questions.A[index]answers[0]);
+    $("#answer2-holder").html(questions.A[index]answers[1]);
+    $("#answer3-holder").html(questions.A[index]answers[2]);
+    $("#answer4-holder").html(questions.A[index]answers[3]);
+  }
+}
+
+function nextQuestion() {
+  //  TODO: Increment the count by 1.
+  count2++;
+
+  $("#image-holder").html("<img src='images/loading.gif' width='200px'/>");
+  // TODO: Use a setTimeout to run displayImage after 1 second.
+  setTimeout(displayQAndA, 1000);
+
+
+  // TODO: If the count is the same as the length of the question array, reset the count to 0.
+  if (count2 === stopwatch.length) {
+    count2 = 0;
+  }
+}
+
+function startTriviaGame() {
+
+  // TODO: Use showImage to hold the setInterval to run nextImage.
+  showQuestion = setInterval(nextQuestion, stopwatch.time);
+
+}
+
+function stopTriviaGame() {
+
+  // TODO: Put our clearInterval here:
+  clearInterval(showQuestion);
+
+}
+
+// This will run the display image function as soon as the page loads.
+displayQAndA();
+
